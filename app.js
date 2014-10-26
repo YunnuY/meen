2f3cc3c9432ca96e6949cf3a6fc1143c7ecc346d
feitian124 <feitian124@gmail.com>
2014-10-26 21:28:55 +0800

    return index html except api routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
@@ -20,10 +19,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/dist')));
 
-app.use('/', routes);
-app.use('/users', users);
+
+app.use('/api/users', users);
+
+app.use('/*', function(req, res) {
+  res.sendfile('public/dist/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
