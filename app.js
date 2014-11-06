d5a252d8b7f0c1062c89abea27a3e651c3877cbf
feitian124 <feitian124@gmail.com>
2014-11-06 22:49:17 +0800

    rm duplicated cookieParser

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var cookieParser = require('cookie-parser');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
 app.use('/*', function(req, res) {
