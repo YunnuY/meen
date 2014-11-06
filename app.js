bff728d3ab3e86f39ec8b52b965513a26eecf2b9
feitian124 <feitian124@gmail.com>
2014-11-06 23:19:02 +0800

    mv require logic to app

--- app.js
+++ app.js
@@ -1,7 +1,47 @@
 var express = require('express');
-var path = require('path');
+var fs = require('fs');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var config = require('config');
 
 var app = express();
+
+// Connect to mongodb
+var connect = function () {
+  var options = { server: { socketOptions: { keepAlive: 1 } } };
+  mongoose.connect(config.db, options);
+};
+connect();
+
+mongoose.connection.on('error', console.log);
+mongoose.connection.on('disconnected', connect);
+
+// Bootstrap models
+fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+  if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+});
+
+// Bootstrap passport config
+require('./config/passport')(passport, config);
+
+// Bootstrap application settings
+require('./config/express')(app, passport);
+
+// Bootstrap routes
+require('./config/routes')(app, passport);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // catch 404 and forward to error handler
