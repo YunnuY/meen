29b1c9afc1e4d259120378bd40574cc328b2a655
feitian124 <feitian124@gmail.com>
2014-11-06 22:50:59 +0800

    rm serve-favicon

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 
 var app = express();
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
