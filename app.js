1e2ff3068ed68a2d07a07d7e831f0d911c3a380b
feitian124 <feitian124@gmail.com>
2014-11-06 22:39:25 +0800

    remove use jade

--- app.js
+++ app.js
@@ -5,13 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var users = require('./app/routes/users');
-
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -20,9 +14,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/dist')));
-
-
-app.use('/api/users', users);
 
 app.use('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/dist/index.html'));
