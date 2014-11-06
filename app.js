86b67d01a7a13257c9c2a5f25b5ff8e5aaf6bc01
feitian124 <feitian124@gmail.com>
2014-11-06 22:47:39 +0800

    rm morgan

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public/dist')));
 
