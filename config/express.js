29d51d366c8e8ef16749921cab9c4c3c9728071d
feitian124 <feitian124@gmail.com>
2014-11-08 00:19:47 +0800

    res.send no need root and format code

--- config/express.js
+++ config/express.js
@@ -103,29 +103,29 @@ module.exports = function (app, passport) {
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
-      var err = new Error('Not Found');
-      err.status = 404;
-      next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
   });
 
   // development error handler, will print stacktrace
   if (app.get('env') === 'development') {
-      app.use(function(err, req, res, next) {
-          res.status(err.status || 500);
-          res.send({
-              message: err.message,
-              error: err
-          });
+    app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.send({
+        message: err.message,
+        error: err
       });
+    });
   }
 
   // production error handler, no stacktraces leaked to user
   app.use(function(err, req, res, next) {
-      res.status(err.status || 500);
-      res.send('error', {
-          message: err.message,
-          error: {}
-      });
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: {}
+    });
   });
 };
 
