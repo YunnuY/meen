ba9d36d2547320c29a7ff006a36a752a64dfd953
feitian124 <feitian124@gmail.com>
2014-11-11 22:39:26 +0800

    disable csrf

--- config/express.js
+++ config/express.js
@@ -99,14 +99,14 @@ module.exports = function (app, passport) {
   app.use(helpers(pkg.name));
 
   // adds CSRF support
-  if (process.env.NODE_ENV !== 'test') {
-    app.use(csrf());
+  // if (process.env.NODE_ENV !== 'test') {
+  //   app.use(csrf());
 
-    // This could be moved to view-helpers :-)
-    app.use(function(req, res, next){
-      res.locals.csrf_token = req.csrfToken();
-      next();
-    });
-  }
+  //   // This could be moved to view-helpers :-)
+  //   app.use(function(req, res, next){
+  //     res.locals.csrf_token = req.csrfToken();
+  //     next();
+  //   });
+  // }
 };
 
